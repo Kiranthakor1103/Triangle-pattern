57ab75e4a78f4ddf4a7659c5b257066459efd92c
THAKOR KIRANKUMAR RANJITSINH <thakorkiran1012003@gmail.com>
2024-12-23 12:49:17 +0530

    Initial commit

---
+++ patternP.js
@@ -0,0 +1,75 @@
+const button = document.getElementById("generateButton");
+const button1 = document.getElementById("generateButton1")
+
+  
+  function btnUp () {
+  const num = parseInt(document.getElementById("rows").value);
+  const output = document.getElementById("output"); // Get the output element
+
+  if (!isNaN(num) && num > 0) {
+    output.textContent = ""; // Clear previous output
+    let pattern = "";
+
+      for (let i = num; i >= 1; i -= 1) {
+        let spaces = "";
+        let stars = "";
+
+        // Generate spaces using a for loop
+        for (let j = 0; j <num - i; j++) {
+          spaces += " "; // every row first space
+        }
+
+        // Generate stars using a for loop
+        for (let k = 0; k < i; k++) {
+          stars += "*"=" "; // print star + space
+        }
+
+        pattern += spaces + stars + spaces+ "\n"; // Add spaces and stars
+      }
+      output.textContent = pattern; // Display the pattern on the webpage
+      console.log(pattern);
+   
+    }
+   else {
+    output.textContent = "Please enter a valid positive number."; // Error message
+  }
+}
+
+  function btnDown () {
+  const num = parseInt(document.getElementById("rows").value);
+  const output = document.getElementById("output"); // Get the output element
+
+  if (!isNaN(num) && num > 0) {
+    output.textContent = ""; // Clear previous output
+    let pattern = "";
+
+      // Odd number: standard odd pattern
+      for (let i = 1; i<=num; i += 1) {
+        let spaces = "";
+        let stars = "";
+
+        // Generate spaces using a for loop
+        for (let j = 0; j <num - i; j++) {
+          spaces += " "; // every row first space
+        }
+
+        // Generate stars using a for loop
+        for (let k = 0; k < i; k++) {
+          stars += "*"+" ";// print star + space
+        }
+
+        pattern += spaces + stars +spaces+ "\n"; // Add spaces and stars
+      }
+      output.textContent = pattern; // Display the pattern on the webpage
+      console.log(pattern);
+    }
+   
+  else{
+    output.textContent = "Please enter a valid positive number."; // Error message
+  }
+  }
+
+button1.addEventListener("click",btnUp);
+button.addEventListener("click", btnDown);
+
+
